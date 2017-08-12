23573cdda8b137aebf8023e554d75aeebe2a5325
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-08-12 11:25:52 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use std::collections::HashMap;
+use std::fmt;
+use std::io::stdin;
+
+fn main() {
+    let text_adv = Adventure {
+        stages: {
+            let mut temp = HashMap::new();
+
+            temp.insert("BEGIN".to_owned(), Stage {
+                info: "The beginning.".to_owned(),
+                nexts: vec![(String::new(), String::new()), ("foo".to_owned(), "Middle".to_owned())],
+            });
+            temp.insert("Middle".to_owned(), Stage {
+                info: "The middle.".to_owned(),
+                nexts: vec![("ix".to_owned(), String::new()), ("bar".to_owned(), "END".to_owned())],
+            });
+            temp
+        },
+    };
+
+    text_adv.run();
+}
+
+struct Adventure {
+    stages: HashMap<String, Stage>
+}
+
+impl Adventure {
+    fn run(&self) {
+        let mut stage_next = self.stages.get("BEGIN"); 
+        
+        loop {
+            let stage = match stage_next {
+                Some(s) => {
+                    println!("{}", s);
+                    s
+                },
+                None    => {
+                    println!("\n\n    The End.\n\n");
+                    break;
+                },
+            };
+
+            stage_next = loop {
+                let choice = loop {
+                    let mut choice = String::new();
+                    
+                    stdin().read_line(&mut choice).unwrap();
+                    
+                    if let Ok(n) = choice.trim().parse::<usize>() {
+                        break n;
+                    } else {
+                        eprintln!("That cannot be parsed as a number.");
+                    }
+                } - 1;
+
+                if let Some(&(_, ref next)) = stage.nexts.get(choice) {
+                    break self.stages.get(next);
+                } else {
+                    eprintln!("There aren't that many choices!");
+                }
+            }
+        }
+    }
+}
+
+struct Stage {
+    info: String,
+    nexts: Vec<(String, String)>,
+}
+
+impl fmt::Display for Stage {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}\n{}", self.info,
+            self.nexts.iter().enumerate()
+                .map(|(i, &(ref c, _))| format!("    {}. {}\n", &i + 1, c))
+                .collect::<String>()
+        )
+    }
+}
